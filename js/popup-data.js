8143214fa5c204322381ce4cf4b896d1343b01f9
alpinehome-jcha <alpinehome.jcha@gmail.com>
2026-02-12 13:10:12 +0900

    Update popup data from Admin Panel

--- js/popup-data.js
+++ js/popup-data.js
@@ -1,13 +1,4 @@
-const initialPopupData = [
-    {
-        "id": 1770694714225,
-        "title": "오키나와렌트",
-        "isActive": true,
-        "imagePath": "assets/images/popupimage/01.jpg",
-        "hideDays": 360,
-        "linkUrl": "https://car.okinawaobaksa.com/kr/okinawa/shop/car_arusu"
-    }
-];
+const initialPopupData = [];
 
 let popupData = [];
 if (typeof localStorage !== 'undefined') {
