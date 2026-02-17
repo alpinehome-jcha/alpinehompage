51c13223345bc0b8a76f3d07997531db79ac06a2
alpinehome-jcha <alpinehome.jcha@gmail.com>
2026-02-17 14:13:30 +0900

    Auto-save product data

--- js/product-data.js
+++ js/product-data.js
@@ -2622,6 +2622,11 @@ const initialProductData = [
                 "id": 1771042189893.2913,
                 "type": "text",
                 "value": "20년~23년 : 9채널(Harman) : 630 / 540 / X7\n\n24년 이후 : 9채널(Bower & Wilkins) : 630 / 540 / X7\n\nDSP : 차측 케이블이므로 DSP용 케이블을 추가 구매해야합니다"
+            },
+            {
+                "id": 1771305200247.2854,
+                "type": "image",
+                "value": "assets/images/BM-2A-C.jpg"
             }
         ],
         "attachments": []
@@ -3190,7 +3195,7 @@ const initialProductData = [
         "attachments": []
     }
 ];
-const PRODUCT_DATA_VERSION = 1771305037423;
+const PRODUCT_DATA_VERSION = 1771305207945;
 
 let productData = [];
 if (typeof localStorage !== 'undefined') {
