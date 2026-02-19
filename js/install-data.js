f5d62f43d10ec1a16ebe7b0fd0382b25a621d5de
alpinehome-jcha <alpinehome.jcha@gmail.com>
2026-02-19 11:37:46 +0900

    Auto-save install data

--- js/install-data.js
+++ js/install-data.js
@@ -136,7 +136,7 @@ const initialInstallData = [
         "author": "Admin"
     }
 ];
-const INSTALL_DATA_VERSION = 1771468569411;
+const INSTALL_DATA_VERSION = 1771468666783;
 
 let installData = [];
 if (typeof localStorage !== 'undefined') {
