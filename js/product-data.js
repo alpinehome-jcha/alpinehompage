28adae9e9a4c6f80297f5eba95c6db31eba57ccf
alpinehome-jcha <alpinehome.jcha@gmail.com>
2026-02-19 15:35:02 +0900

    Auto-save product data

--- js/product-data.js
+++ js/product-data.js
@@ -3001,7 +3001,7 @@ const initialProductData = [
         "desc_bottom": "B타입 / DSP측 케이블",
         "price": 180000,
         "soldOut": true,
-        "image": "assets/images/product_placeholder.png",
+        "image": "https://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/DS-10B-2.jpg",
         "detailBlocks": [
             {
                 "id": 1771337271863.7468,
@@ -3311,7 +3311,7 @@ const initialProductData = [
         "attachments": []
     }
 ];
-const PRODUCT_DATA_VERSION = 1771482878871;
+const PRODUCT_DATA_VERSION = 1771482901429;
 
 let productData = [];
 if (typeof localStorage !== 'undefined') {
