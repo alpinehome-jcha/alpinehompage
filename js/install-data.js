8ec4b9cf98f3066a8366fb57ca6234aae8d2d83e
alpinehome-jcha <alpinehome.jcha@gmail.com>
2026-02-19 11:50:20 +0900

    Auto-save install data

--- js/install-data.js
+++ js/install-data.js
@@ -104,19 +104,19 @@ const initialInstallData = [
             },
             {
                 "type": "image",
-                "value": "assets/images/amp-m770-1.jpg"
+                "value": "https://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/amp-m770-1.jpg"
             },
             {
                 "type": "image",
-                "value": "assets/images/amp-m770-2.jpg"
+                "value": "https://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/amp-m770-2.jpg"
             },
             {
                 "type": "image",
-                "value": "assets/images/amp-m770-3.jpg"
+                "value": "https://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/amp-m770-3.jpg"
             },
             {
                 "type": "image",
-                "value": "assets/images/amp-m770-4.jpg"
+                "value": "https://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/amp-m770-4.jpg"
             },
             {
                 "type": "text",
@@ -124,19 +124,19 @@ const initialInstallData = [
             },
             {
                 "type": "image",
-                "value": "assets/images/amp-m770-5.jpg"
+                "value": "https://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/amp-m770-5.jpg"
             },
             {
                 "type": "text",
                 "value": "서울/송파 오토갤러리 010-5220-8034 서울시 송파구 새말로 176 트윈팰리스 101동 102호\n인천/서구 썬카펙토리 010-9773-9882 인천 서구 봉수대로 166 2동 101호\n강원/속초 레트로카오디오 010-6879-9255 강원 속초시 청호로 40\n경기/분당 개구리카오디오 010-5283-6277 경기 성남시 분당구 대왕판교로 255\n경기/성남 카투오디오 010-3597-1735 경기 성남시 중원구 둔촌대로64번길 4-16(101호)\n경기/광명 사운드테크닉 010-4764-7777 경기 광명시 금오로 745\n경기/양주 럭셔리카클럽 010-9956-3618 경기 양주시 부흥로 1324\n경기/화성 카오디오팩토리 010-6580-1975 경기 화성시 동탄반송2길 41-7\n전북/전주 퍼니사운드 010-5123-9255 전북 전주시 덕진구 쪽구름2길 34 1층 101\n광주/남구 광주카오디오 010-3627-9072 광주 남구 독립로 163\n제주/제주시 사운드째즈 010-3693-9499 제주 제주시 연삼로 124​\n\n[알파인 대리점]\n서울/서초 바이지츠아우토 010-3656-6276 서울 서초구 명달로 52-7 미양빌딩\n서울/강남 카사운드메이커 010-2618-2455 서울특별시 서초구 강남대로 101안길 36\n경기/구리 현대카오디오 010-3795-4826 경기도 구리시 수택동 430\n경기/하남 바이지츠아우토 010-9092-2692 경기 하남시 대성로169번길 34\n충남/아산 퀄리티 하우스 010-4816-9221 충남 아산시 삼동로 70\n대전/서구 파워뱅크하우스 010-5407-4999 대전 서구 도산로 465\n대구/남구 핸즈카오디오 010-9077-6658 대구 남구 효성로 15 미리내맨션 상가\n전북/익산 카사운드메이커 010-2618-2455 전북 익산시 익산대로 68\n강원/속초 레트로카오디오 010-6879-9255 강원특별자치도 속초시 청호로 40\n부산/강서 사운드닥터 010-4855-8949 부산 강서구 대저중앙로 139"
             }
         ],
-        "content": "무쏘 EV 블랙엣지 차량에 기본 장착된 알파인 오디오는 OEM 4채널 사운드 시스템을 비약적으로 향상 시킨 프리미엄 사운드입니다.\n아쉬운점은 4채널 특성상 서브우퍼가 부재한점입니다.\n\n알파인 서브우퍼 PWE-M770은 300W급 모노앰프가 내장된 7\" 고성능 제품으로 공간이 협소한 KGM 무쏘EV 차량에 알맞은 제품입니다.\n\nSubwoofer Type : 7 x 7\" Powered Subwoofer Box\nFrequency response : 10 Hz - 150 Hz\nPower handling : 300 Watts peak power\nSensitivity : 90dB\n\n<PWE-M770 제품 링크>\nhttps://www.alpine-korea.co.kr/pages/detail.html?id=1770338317503\nhttps://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/amp-m770-0.jpg\n무쏘 EV 오너들이 알파인 서브우포 PWE-M770을 선택하는이유?\n1. 순정 인테리어를 해치지 않는 디자인으로, 트렁크 공간 무손상 시공 탈부착이 간편하며, 순정 감성을 그대로 유지합니다.\n2. 강력하면서도 균형잡힌 저역 , 알파인 서브우퍼는 도어 스피커와의 밸런스를 고려한 트닝으로 묵직하지만 퍼지지 않는 저음, 풍부하면서도 정제된 저역을 구현합니다.\n3. KGM의 알파인 오디오와의 호환성, RCA단자로 연결되며, 알파인 오디오의 DSP AMP에는 알파인 서브우퍼 PWE-M770을 튜닝할 수 있도록 구성되어 있습니다.\n4. 컨트롤러 장착, 베이스 조절 컴트롤러를 장착해서 주행 중에소 음악에 따라 손쉽게 조절할 수 있습니다.\nassets/images/amp-m770-1.jpg\nassets/images/amp-m770-2.jpg\nassets/images/amp-m770-3.jpg\nassets/images/amp-m770-4.jpg\nKGM 알파인 오디오는 사운드 튜닝이 가능한 상품이므로, DSP와 스피커를 다른 상품으로 변경 할 필요가 없습니다.\nKGM AS 이슈가 발생 할 수 있으니 알파인 DSP와 알파인 스피커는 다른 상품으로 변경하지 마실 것을 권장합니다.\n다만 부족한 저음은 아래의 상품을 추가하여 사운드 튜닝하시면 충분히 만족 하실 것입니다.\nassets/images/amp-m770-5.jpg\n서울/송파 오토갤러리 010-5220-8034 서울시 송파구 새말로 176 트윈팰리스 101동 102호\n인천/서구 썬카펙토리 010-9773-9882 인천 서구 봉수대로 166 2동 101호\n강원/속초 레트로카오디오 010-6879-9255 강원 속초시 청호로 40\n경기/분당 개구리카오디오 010-5283-6277 경기 성남시 분당구 대왕판교로 255\n경기/성남 카투오디오 010-3597-1735 경기 성남시 중원구 둔촌대로64번길 4-16(101호)\n경기/광명 사운드테크닉 010-4764-7777 경기 광명시 금오로 745\n경기/양주 럭셔리카클럽 010-9956-3618 경기 양주시 부흥로 1324\n경기/화성 카오디오팩토리 010-6580-1975 경기 화성시 동탄반송2길 41-7\n전북/전주 퍼니사운드 010-5123-9255 전북 전주시 덕진구 쪽구름2길 34 1층 101\n광주/남구 광주카오디오 010-3627-9072 광주 남구 독립로 163\n제주/제주시 사운드째즈 010-3693-9499 제주 제주시 연삼로 124​\n\n[알파인 대리점]\n서울/서초 바이지츠아우토 010-3656-6276 서울 서초구 명달로 52-7 미양빌딩\n서울/강남 카사운드메이커 010-2618-2455 서울특별시 서초구 강남대로 101안길 36\n경기/구리 현대카오디오 010-3795-4826 경기도 구리시 수택동 430\n경기/하남 바이지츠아우토 010-9092-2692 경기 하남시 대성로169번길 34\n충남/아산 퀄리티 하우스 010-4816-9221 충남 아산시 삼동로 70\n대전/서구 파워뱅크하우스 010-5407-4999 대전 서구 도산로 465\n대구/남구 핸즈카오디오 010-9077-6658 대구 남구 효성로 15 미리내맨션 상가\n전북/익산 카사운드메이커 010-2618-2455 전북 익산시 익산대로 68\n강원/속초 레트로카오디오 010-6879-9255 강원특별자치도 속초시 청호로 40\n부산/강서 사운드닥터 010-4855-8949 부산 강서구 대저중앙로 139",
+        "content": "무쏘 EV 블랙엣지 차량에 기본 장착된 알파인 오디오는 OEM 4채널 사운드 시스템을 비약적으로 향상 시킨 프리미엄 사운드입니다.\n아쉬운점은 4채널 특성상 서브우퍼가 부재한점입니다.\n\n알파인 서브우퍼 PWE-M770은 300W급 모노앰프가 내장된 7\" 고성능 제품으로 공간이 협소한 KGM 무쏘EV 차량에 알맞은 제품입니다.\n\nSubwoofer Type : 7 x 7\" Powered Subwoofer Box\nFrequency response : 10 Hz - 150 Hz\nPower handling : 300 Watts peak power\nSensitivity : 90dB\n\n<PWE-M770 제품 링크>\nhttps://www.alpine-korea.co.kr/pages/detail.html?id=1770338317503\nhttps://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/amp-m770-0.jpg\n무쏘 EV 오너들이 알파인 서브우포 PWE-M770을 선택하는이유?\n1. 순정 인테리어를 해치지 않는 디자인으로, 트렁크 공간 무손상 시공 탈부착이 간편하며, 순정 감성을 그대로 유지합니다.\n2. 강력하면서도 균형잡힌 저역 , 알파인 서브우퍼는 도어 스피커와의 밸런스를 고려한 트닝으로 묵직하지만 퍼지지 않는 저음, 풍부하면서도 정제된 저역을 구현합니다.\n3. KGM의 알파인 오디오와의 호환성, RCA단자로 연결되며, 알파인 오디오의 DSP AMP에는 알파인 서브우퍼 PWE-M770을 튜닝할 수 있도록 구성되어 있습니다.\n4. 컨트롤러 장착, 베이스 조절 컴트롤러를 장착해서 주행 중에소 음악에 따라 손쉽게 조절할 수 있습니다.\nhttps://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/amp-m770-1.jpg\nhttps://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/amp-m770-2.jpg\nhttps://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/amp-m770-3.jpg\nhttps://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/amp-m770-4.jpg\nKGM 알파인 오디오는 사운드 튜닝이 가능한 상품이므로, DSP와 스피커를 다른 상품으로 변경 할 필요가 없습니다.\nKGM AS 이슈가 발생 할 수 있으니 알파인 DSP와 알파인 스피커는 다른 상품으로 변경하지 마실 것을 권장합니다.\n다만 부족한 저음은 아래의 상품을 추가하여 사운드 튜닝하시면 충분히 만족 하실 것입니다.\nhttps://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/amp-m770-5.jpg\n서울/송파 오토갤러리 010-5220-8034 서울시 송파구 새말로 176 트윈팰리스 101동 102호\n인천/서구 썬카펙토리 010-9773-9882 인천 서구 봉수대로 166 2동 101호\n강원/속초 레트로카오디오 010-6879-9255 강원 속초시 청호로 40\n경기/분당 개구리카오디오 010-5283-6277 경기 성남시 분당구 대왕판교로 255\n경기/성남 카투오디오 010-3597-1735 경기 성남시 중원구 둔촌대로64번길 4-16(101호)\n경기/광명 사운드테크닉 010-4764-7777 경기 광명시 금오로 745\n경기/양주 럭셔리카클럽 010-9956-3618 경기 양주시 부흥로 1324\n경기/화성 카오디오팩토리 010-6580-1975 경기 화성시 동탄반송2길 41-7\n전북/전주 퍼니사운드 010-5123-9255 전북 전주시 덕진구 쪽구름2길 34 1층 101\n광주/남구 광주카오디오 010-3627-9072 광주 남구 독립로 163\n제주/제주시 사운드째즈 010-3693-9499 제주 제주시 연삼로 124​\n\n[알파인 대리점]\n서울/서초 바이지츠아우토 010-3656-6276 서울 서초구 명달로 52-7 미양빌딩\n서울/강남 카사운드메이커 010-2618-2455 서울특별시 서초구 강남대로 101안길 36\n경기/구리 현대카오디오 010-3795-4826 경기도 구리시 수택동 430\n경기/하남 바이지츠아우토 010-9092-2692 경기 하남시 대성로169번길 34\n충남/아산 퀄리티 하우스 010-4816-9221 충남 아산시 삼동로 70\n대전/서구 파워뱅크하우스 010-5407-4999 대전 서구 도산로 465\n대구/남구 핸즈카오디오 010-9077-6658 대구 남구 효성로 15 미리내맨션 상가\n전북/익산 카사운드메이커 010-2618-2455 전북 익산시 익산대로 68\n강원/속초 레트로카오디오 010-6879-9255 강원특별자치도 속초시 청호로 40\n부산/강서 사운드닥터 010-4855-8949 부산 강서구 대저중앙로 139",
         "files": [],
         "author": "Admin"
     }
 ];
-const INSTALL_DATA_VERSION = 1771469361774;
+const INSTALL_DATA_VERSION = 1771469419523;
 
 let installData = [];
 if (typeof localStorage !== 'undefined') {
