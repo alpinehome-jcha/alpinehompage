25c6bfd715365cd60c6a865f6178f0a0694914ed
alpinehome-jcha <alpinehome.jcha@gmail.com>
2026-02-21 21:41:26 +0900

    Update estimate data from Admin/Resource Panel

--- js/estimate-data.js
+++ js/estimate-data.js
@@ -7961,7 +7961,7 @@ const initialEstimateData = [
         "surround": []
     }
 ];
-const ESTIMATE_DATA_VERSION = 1771677175490;
+const ESTIMATE_DATA_VERSION = 1771677684480;
 
 let estimateData = [];
 if (typeof localStorage !== 'undefined') {
