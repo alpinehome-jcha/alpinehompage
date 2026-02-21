617189dd2bece1025ab46543aa0a431c6b573169
alpinehome-jcha <alpinehome.jcha@gmail.com>
2026-02-21 21:32:42 +0900

    Auto-save estimate data

--- js/estimate-data.js
+++ js/estimate-data.js
@@ -7961,7 +7961,7 @@ const initialEstimateData = [
         "surround": []
     }
 ];
-const ESTIMATE_DATA_VERSION = 1771677069904;
+const ESTIMATE_DATA_VERSION = 1771677160862;
 
 let estimateData = [];
 if (typeof localStorage !== 'undefined') {
