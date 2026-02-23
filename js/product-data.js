506e000b3fe91cf424aa6f843f34d3e6b1d95892
alpinehome-jcha <alpinehome.jcha@gmail.com>
2026-02-23 14:05:09 +0900

    Auto-save product data

--- js/product-data.js
+++ js/product-data.js
@@ -1993,7 +1993,7 @@ const initialProductData = [
         "title": "EV-40M-T",
         "desc": "Tesla 4\" Front Dashboard Midrange Speaker Set",
         "desc_bottom": ".",
-        "price": 560000,
+        "price": 590000,
         "soldOut": false,
         "image": "assets/images/EV-40MT.jpg",
         "detailBlocks": [
@@ -3390,7 +3390,7 @@ const initialProductData = [
         "attachments": []
     }
 ];
-const PRODUCT_DATA_VERSION = 1771823086349;
+const PRODUCT_DATA_VERSION = 1771823108099;
 
 let productData = [];
 if (typeof localStorage !== 'undefined') {
