89f024c03181a8992e1951dc4fcad3e1921db7cb
alpinehome-jcha <alpinehome.jcha@gmail.com>
2026-02-25 13:35:49 +0900

    Update PnP Search Data via Admin Setup

--- js/pnp-search-data.js
+++ js/pnp-search-data.js
@@ -1,7 +1,7 @@
 /**
  * PnP Cable Search Data
  * Hierarchy: Manufacturer -> Model -> Detail (Year/Code/Sound) -> Products
- * Updated: 2026. 2. 25. 오후 1:33:58
+ * Updated: 2026. 2. 25. 오후 1:35:49
  */
 const pnpSearchData = [
     {
@@ -1418,6 +1418,32 @@ const pnpSearchData = [
                     {
                         "year": "25년 이후",
                         "code": "O100",
+                        "sound": "알파인오디오",
+                        "integrated": "X",
+                        "typeA": "KG-1A"
+                    }
+                ]
+            },
+            {
+                "model": "토레스",
+                "details": [
+                    {
+                        "year": "~23년 이전",
+                        "code": "J100",
+                        "sound": "4채널 자출",
+                        "integrated": "HK-102",
+                        "typeA": "X"
+                    },
+                    {
+                        "year": "24년 이후",
+                        "code": "J100",
+                        "sound": "4채널 자출",
+                        "integrated": "HK-103",
+                        "typeA": "HK-1A"
+                    },
+                    {
+                        "year": "25년 이후",
+                        "code": "J100",
                         "sound": "알파인오디오",
                         "integrated": "X",
                         "typeA": "KG-1A"
