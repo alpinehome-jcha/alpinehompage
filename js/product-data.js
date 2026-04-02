202dfdc70587b813e8435157e6c72adeb75660e8
alpinehome-jcha <alpinehome.jcha@gmail.com>
2026-04-02 14:22:45 +0900

    Auto-save product data

--- js/product-data.js
+++ js/product-data.js
@@ -3331,7 +3331,7 @@ const initialProductData = [
         "title": "G80 7채널 액튠_알파인 사운드 패키지(무손상 OEM Fit)",
         "desc": "당신의 G80을 위한 단 하나의 '사운드 큐레이션'",
         "desc_bottom": "액튠의 한계를 넘어, 알파인 S-시리즈의 섬세한 디테일과 10채널 DSP의 정밀함이 만났습니다.",
-        "price": 3630000,
+        "price": 3442000,
         "soldOut": false,
         "image": "https://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/images/G80%20actune.jpg",
         "detailBlocks": [
@@ -4120,7 +4120,7 @@ const initialProductData = [
         "attachments": []
     }
 ];
-const PRODUCT_DATA_VERSION = 1775093387829;
+const PRODUCT_DATA_VERSION = 1775107363432;
 
 let productData = [];
 if (typeof localStorage !== 'undefined') {
