030f466c0824a3d160e93a5e883727b7f1d50248
alpinehome-jcha <alpinehome.jcha@gmail.com>
2026-05-26 11:25:57 +0900

    Auto-save resource data

--- js/resource-data.js
+++ js/resource-data.js
@@ -1,14 +1,20 @@
 const initialResourceData = [
     {
-        "id": 1773204679768,
-        "date": "2026-03-11",
-        "title": "현대/기아/제네시스 노이즈 캔슬링 마이크 위치",
-        "content": "현대/기아/제네시스에서 노이즈 캔슬링이 장착되 차종\n\nG80\nG90\nGV60\nGV70\nGV80\n펠리세이드(LX2)24년이전/ (LX3)25년이후에는없음\nEV9\n아이오닉9\nK8\n그랜저(GN7)\n\n노이즈 캔슬링기능은 끌수 없습니다. OEM앰프에서 마이크 배선을 자르는 방법도 있지만 일부 차종은 마이크 전용 배선이 없을 수 있습니다.\n가장 확실한 방법은 노이즈 캔슬링 마이크를 탈거해서 마이크에 소리가 입력되지 않도록 테잎등으로 감싸는 것입니다.\n\n첨부파일은 각 차종의 노이즈 캔슬링 마이크 위치입니다.",
-        "image": "",
+        "id": 1779762359006,
+        "date": "2026-05-26",
+        "title": "그랜저 GN7 자출(4채널)차량 Preset 및 인스톨 설명",
+        "content": "그랜저 GN7 자출(4채널)차량 Preset 데이터입니다.\n\n1. 첨부된 파일을 다운로드 하십시오.\n\n2. 대리점 자료실에서 PXE-M60-4 PC 소프트웨어를 다운로드하시고 실행하십시오.",
+        "image": "assets/images/서브우퍼.jpg",
         "files": [
             {
-                "name": "20260311 현대기아 RANC MIC.pdf",
-                "path": "assets/files/20260311 현대기아 RANC MIC.pdf"
+                "name": "4CH+Sub_W_PXE-M60-4_0712_GN74CH.jssh",
+                "path": "assets/files/4CH+Sub_W_PXE-M60-4_0712_GN74CH.jssh",
+                "url": "https://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/files/4CH+Sub_W_PXE-M60-4_0712_GN74CH.jssh"
+            },
+            {
+                "name": "GN7 4채널 M60_M770설치메뉴얼.pdf",
+                "path": "assets/files/GN7 4채널 M60_M770설치메뉴얼.pdf",
+                "url": "https://raw.githubusercontent.com/alpinehome-jcha/alpinehompage/main/assets/files/GN7 4채널 M60_M770설치메뉴얼.pdf"
             }
         ],
         "author": "Admin"
@@ -42,7 +48,7 @@ const initialResourceData = [
         "author": "Admin"
     }
 ];
-const RESOURCE_DATA_VERSION = 1773207209507;
+const RESOURCE_DATA_VERSION = 1779762360306;
 
 let resourceData = [];
 if (typeof localStorage !== 'undefined') {
