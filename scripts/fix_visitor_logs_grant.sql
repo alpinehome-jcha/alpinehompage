c7ca1a635821b65553f7f35cc97b3d3aa00a232e
alpinehome-jcha <alpinehome.jcha@gmail.com>
2026-09-07 13:55:34 +0900

    fix: visitor_logs 테이블 id 시퀀스 오류 수정 (setval)

--- scripts/fix_visitor_logs_grant.sql
+++ scripts/fix_visitor_logs_grant.sql
@@ -1,1 +1,2 @@
 GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE "alpine-home".visitor_logs TO anon, authenticated, service_role;
+SELECT setval(pg_get_serial_sequence('"alpine-home".visitor_logs', 'id'), coalesce(max(id),0) + 1, false) FROM "alpine-home".visitor_logs;
